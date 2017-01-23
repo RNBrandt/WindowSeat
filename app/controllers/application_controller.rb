9a09a6932fd8137af5723a63094a9d5aa8d06531
Reuben Brandt <Reubennbrandt@gmail.com>
2017-01-23 13:07:05 -0800

    Add phone number and name to user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_filter :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
+
+  def configure_permitted_parameters
+    added_attrs = [:first_name, :last_name, :phone_number]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,4 +3,23 @@ class Flight < ApplicationRecord
   validates :flight_time, presence: true
   validates :checkin_time, presence: true
   belongs_to :user
+
+  after_create :automatic_checkin
+
+  def automatic_checkin
+    #do all the stuff to checkin online
+  end
+
+  def text_reminder
+    @twilio_number = ENV['TWILIO_NUMBER']
+    @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
+    message = "this will change depending on success or failure"
+    time_string = ((self.checkin_time).localtime).strftime("%I:%M%p on %b. %d, %Y")
+    message = @client.account.messages.create(
+      from: @twilio_number,
+      to: self.user.phone_number)
+      # body: )
+  end
+
+
 end
