553945df5ea84a41d3436eb209e014d77e38161d
Reuben Brandt <Reubennbrandt@gmail.com>
2017-03-06 22:11:46 -0800

    create views and refine testing

--- spec/workers/flight_worker_spec.rb
+++ spec/workers/flight_worker_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe FlightWorker, type: :worker do
+
   it "will enqueue a worker when called" do
     expect{ FlightWorker.perform_async(1) }.to change{ FlightWorker.jobs.size }
   end
