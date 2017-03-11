d3c9d6cef163b7eb118098c91f88966fbd7728c3
Reuben Brandt <Reubennbrandt@gmail.com>
2017-03-11 14:51:39 -0800

    change migrations to fix heroku error?

--- db/migrate/20170116005913_add_flight_info_to_flights.rb
+++ db/migrate/20170116005913_add_flight_info_to_flights.rb
@@ -3,5 +3,6 @@ class AddFlightInfoToFlights < ActiveRecord::Migration[5.0]
     add_column :flights, :flight_number, :string
     add_column :flights, :flight_time, :datetime
     add_column :flights, :checkin_time, :datetime
+    add_column :flights, :timezone, :string
   end
 end
--- db/migrate/20170310203128_add_time_zones_to_flight.rb
+++
@@ -1,5 +0,0 @@
-class AddTimeZonesToFlight < ActiveRecord::Migration[5.0]
-  def change
-    add_column :flights, :timezone, :string
-  end
-end
