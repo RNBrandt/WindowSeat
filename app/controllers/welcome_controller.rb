d17a64886a14dd45ed329832dcfe1e8863c0738b
cvilla100@gmail.com <cvilla100@gmail.com>
2017-03-18 20:30:24 -0700

    Change root to welcome#index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    if current_user
+      redirect_to new_user_flight_path(current_user)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
         sessions: 'users/sessions'
       }
-  get 'welcome/index'
 
   resources :user do
     resources :flights
@@ -10,6 +9,5 @@ Rails.application.routes.draw do
 
   resources :charges
 
-  root 'flights#new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
 end
